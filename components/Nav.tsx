b71183692c79c32a04b9acfabb04d180454eb229
JTris1 <johnathontrisler@gmail.com>
2023-05-18 18:14:08 -0400

    Started working with ThreeJS

---
+++ components/Nav.tsx
@@ -0,0 +1,27 @@
+import React from "react";
+import Link from "next/link";
+import { McLaren } from "~/components/icons";
+import Image from "next/image";
+
+type Props = {};
+
+export default function Nav({}: Props) {
+  return (
+    <nav className="flex items-center w-full h-20 px-16">
+      <ul className="inline-flex items-center w-full justify-between list-none">
+        <div>
+          <li className="">
+            <Link href="/" className="overflow-hidden">
+              <McLaren className="fill-white h-full w-36" />
+            </Link>
+          </li>
+        </div>
+
+        <div className="flex">
+          <li className="font-normal m-5 [letter-spacing:1px]">CARS</li>
+          <li className="font-normal m-5 [letter-spacing:1px]">ABOUT</li>
+        </div>
+      </ul>
+    </nav>
+  );
+}
